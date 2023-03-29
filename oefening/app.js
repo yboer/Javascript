821821b5456ed28f0f167dbd339a80651441cea2
yboer <9018396@student.zadkine.nl>
2023-03-29 09:51:43 +0200

    opdraht kar

---
+++ oefening/app.js
@@ -0,0 +1,4 @@
+document.getElementById("title").style.color = "blue";
+document.getElementById("title").style.fontWeight = "bold";
+//haal het element p op//
+document.getElementById("para").innerText = "uitslover";
