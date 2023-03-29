02f57fe659c464be11cee6cd9ace67cdf2ce9738
yboer <9018396@student.zadkine.nl>
2023-03-29 10:30:40 +0200

    opdracht 5.1

---
+++ 6.1/app.js
@@ -0,0 +1,6 @@
+function kleur_aanpassen(){
+    document.getElementById("title").style.color = "blue";
+document.getElementById("title").style.fontWeight = "bold";
+document.getElementById("para").innerText = "uitslover";
+}
+
