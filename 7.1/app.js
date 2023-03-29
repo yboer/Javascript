0ec2a7edf6e105eef23749076bed6716fd97bdc2
yboer <9018396@student.zadkine.nl>
2023-03-29 21:03:16 +0200

    opdracht 7.1 denk ik

---
+++ 7.1/app.js
@@ -0,0 +1,27 @@
+const dice1 = 100
+const dice2 = 60
+
+
+
+
+ 
+
+
+if (dice1 > 60 && dice2 ===60) {
+    console.log("U have enough money")
+}
+
+const budget = 100
+
+if(budget ===100) {
+    console.log("je hebt genoeg geld")
+} else {
+    console.log("je hebt niet genoeg geld")
+}
+
+const product = 60
+if(product === 60) {
+    console.log("Thank you for your purchase")
+} else {
+    console.log("Sorry, you dont have enough money to buy this.. what are you even doing here?")
+}
---
+++ variables/app.js
@@ -0,0 +1,12 @@
+const age = 18;
+
+if(age >=        18){
+    console.log("You Are Good To Go!")
+}
+else {
+console.log("You are not old enough")
+}
+
+
+
+
