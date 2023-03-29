e6693d4929c36783d41ad983646854f80b0f4dc9
yboer <9018396@student.zadkine.nl>
2023-03-29 13:47:43 +0200

    opdracht 6.4 gemaakt

---
+++ 6.4/app.js
@@ -0,0 +1,9 @@
+var num1 = window.prompt("Enter a number");
+var num2 = window.prompt("Enter another number");
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+document.write(num1+num2);
+
+
