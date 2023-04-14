db1b5c1440aa3ceb1cf658c8730f365e982bbc67
yboer <9018396@student.zadkine.nl>
2023-04-14 10:30:38 +0200

    opdracgt 8.1 2 gedaan

--- 8.1 2/app.js
+++ 8.1 2/app.js
@@ -7,23 +7,39 @@ let title = document.getElementById("title");
 
 
 function add() {
+    if(input1.value > 0 && input2.value) {
     let answer = Number(output1.value) + Number(output2.value);
-    title.innerText = answer;
+    title.innerText = answer
+} else {
+    title.innerText = "Getal is te laag"
+}
 }
 
 function minus() {
+    if(input1.value > 0 && input2.value) {
     let answer = output1.value - output2.value;
     title.innerText = answer;
+} else {
+    title.innerText = "Getal is te laag"
+}
 
 }
 
 function divide() {
+    if(input1.value > 0 && input2.value) {
     let answer = output1.value / output2.value;
     title.innerText = answer;
+} else {
+    title.innertext = answer;
+}
 
 }
 
 function multiply() {
+    if(input1.value > 0 && input2.value) {
  let answer = output1.value * output2.value;
 title.innerText = answer;
+} else {
+    title.innerText = answer;
+}
 }
